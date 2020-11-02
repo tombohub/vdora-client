763dbaf766b272c52108a7d7d6939348401f5a5e
babaroga <mama@mia.com>
2020-11-01 23:20:04 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Input } from "antd";
 function App() {
   return (
     <>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,21 +1,30 @@
 import React from "react";
+import logo from "../assets/images/logo.webp";
 import { Input, Checkbox, Button, Form } from "antd";
 
 export default function Login() {
   return (
     <>
-      <div className="">
-        <Form>
-          <Form.Item>
-            <Input placeholder="shmuka username" />
-          </Form.Item>
-          <Form.Item>
-            <Input.Password placeholder="shmuka password" />
-          </Form.Item>
-          <Checkbox>Remember me</Checkbox>
-          <Button>Que Vdora</Button>
-        </Form>
-      </div>
+      <main className="h-screen w-screen flex justify-center">
+        <div className="w-1/3 shadow p-4 self-center">
+          <div id="logo" className="p-4">
+            <img src={logo} alt="logo" className="my-0 mx-auto" />
+          </div>
+          <input type="text" />
+          <Form>
+            <Form.Item>
+              <Input placeholder="shmuka username" />
+            </Form.Item>
+            <Form.Item>
+              <Input.Password placeholder="shmuka password" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary">Que Vdora</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </main>
     </>
   );
 }
