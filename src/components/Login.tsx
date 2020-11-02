ca299260bc27aa867a5b1fb8f7ddb781d8e7c649
babaroga <mama@mia.com>
2020-11-02 17:46:01 -0500

    login edit

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,7 @@
 import React from "react";
 import logo from "../assets/images/logo.webp";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
+import { TextField, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -30,7 +19,7 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    marginTop: "25px",
   },
 }));
 
@@ -38,65 +27,46 @@ export default function Login() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+    <>
+      <main className="p-24">
+        <div id="container" className="max-w-sm mx-auto">
+          <div id="logo">
+            <img src={logo} className="mx-auto" alt="logo" />
+          </div>
+          <form action="">
+            <TextField
+              id="username"
+              name="username"
+              label="username"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+            />
+            <TextField
+              id="password"
+              label="password"
+              type="password"
+              variant="outlined"
+              className="my-4"
+              required
+              fullWidth
+            />
+            <button className="my-4">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                // style={{ marginTop: "90px" }}
+                className={classes.submit}
+              >
+                Come come
+              </Button>
+            </button>
+          </form>
+        </div>
+      </main>
+    </>
   );
 }
