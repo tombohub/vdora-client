627d1ca64adfe953f2b8a80f04ba7172c4ec81da
babaroga <mama@mia.com>
2020-11-16 22:33:30 -0500

    finish autocomplete

--- src/components/StockTransferForm.jsx
+++ src/components/StockTransferForm.jsx
@@ -1,12 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { Dropdown } from "primereact/dropdown";
 import axios from "axios";
-import { concat } from "@apollo/client";
+import { Dropdown } from "primereact/dropdown";
+import { InputNumber } from "primereact/inputnumber";
+import { AutoComplete } from "primereact/autocomplete";
 
+/**
+ * Form for transfering, moving products (stock) from one location to another
+ * Fetching locations and products from database.
+ */
 export default function StockTransferForm() {
-  const [fromLocation, setFromLocation] = useState("Nooks");
-  const [toLocation, setToLocation] = useState("In-house");
+  //
+  // location from where the product is moved
+  const [fromLocation, setFromLocation] = useState("");
+
+  // destination where the product is moved to
+  const [toLocation, setToLocation] = useState("");
+
+  // product which is being transfered
+  const [product, setProduct] = useState("");
+
+  // quantity of products moved
+  const [quantity, setQuantity] = useState(1);
+
+  // options for Dropdown component
   const [locationOptions, setLocationOptions] = useState([]);
+
+  // options for Autocomplete component
+  const [productOptions, setProductOptions] = useState([]);
+
+  // filtered products suggestions for Autocomplete component while typing
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     /**
@@ -16,7 +39,7 @@ export default function StockTransferForm() {
      * update here too.
      * @param {Response} locations API response with list of available locations
      */
-    function handleResponse(locations) {
+    function handleLocationsResponse(locations) {
       // set locations to state so dropdown is preselected
       setFromLocation(locations[0].name);
       setToLocation(locations[1].name);
@@ -29,11 +52,53 @@ export default function StockTransferForm() {
       setLocationOptions(newLocationOptions);
     }
 
+    // fetch list of product locations
     axios
       .get("http://localhost:8000/inventory/locations/")
-      .then(res => handleResponse(res.data.results))
+      .then(res => handleLocationsResponse(res.data.results))
+      .catch(err => console.error(err));
+
+    /**
+     * turn the products response object into an array of products. Used for autocomplete
+     * component suggestions
+     * @param {response} productsResponse response containing the list of product objects
+     */
+    function handleProductsResponse(productsResponse) {
+      const newProductOptions = productsResponse.map(
+        product => product.name
+      );
+
+      setProductOptions(newProductOptions);
+    }
+
+    // fetch list of products
+    axios
+      .get("http://localhost:8000/inventory/products/")
+      .then(res => handleProductsResponse(res.data.results))
       .catch(err => console.error(err));
   }, []);
+
+  /**
+   * Filters products based on typing in autocomplete input and sets the product suggestions
+   * @param {event} event html event
+   */
+  function filterProducts(e) {
+    const subString = e.query;
+    //setInputValue(subString);
+
+    // filter if product contains typed string
+    if (subString !== "") {
+      const regex = new RegExp(`${subString}`, "i");
+      const newFilteredProducts = productOptions.filter(product =>
+        regex.test(product)
+      );
+      setFilteredProducts(newFilteredProducts);
+    } else {
+      setFilteredProducts([]);
+    }
+  }
+
+  /* --------------------------------- return --------------------------------- */
 
   return (
     <>
@@ -49,6 +114,18 @@ export default function StockTransferForm() {
           value={toLocation}
           options={locationOptions}
           onChange={e => setToLocation(e.value)}
+        />
+        <InputNumber
+          value={quantity}
+          onValueChange={e => setQuantity(e.value)}
+          showButtons
+          min={1}
+        />
+        <AutoComplete
+          value={product}
+          suggestions={filteredProducts}
+          completeMethod={filterProducts}
+          onChange={e => setProduct(e.value)}
         />
       </form>
     </>
--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,16 +1,1 @@
 import axios from "axios";
-
-function fetchNooksPayouts() {
-  axios
-    .get("http://localhost:8000/sales/reports/nooks-payouts", {
-      // auth: {
-      //     username: context.username,
-      //     password: context.password
-      // },
-    })
-    .then(res => {
-      const newPayouts = res.data;
-      setPayouts(newPayouts);
-    })
-    .catch(err => console.error("error:", err));
-}
