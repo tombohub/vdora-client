b8c601760e34805b8f540a66ed6ae79e984edfa7
babaroga <mama@mia.com>
2020-11-21 14:01:58 -0500

    ref

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Main from "./Main";
 import Sidebar from "./Sidebar";
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:8000//";
-axios.defaults.withCredentials = true;
 
 function Admin() {
   return (
