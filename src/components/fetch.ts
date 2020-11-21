6a43825286dca661c715e93925441c540b9d183d
babaroga <mama@mia.com>
2020-11-21 13:23:33 -0500

    fetch

--- src/components/fetch.ts
+++ src/components/fetch.ts
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+function get(apiBase: string, apiPoint: string) {
+    axios.get(apiBase+apiPoint,{
+        withCredentials: true
+    }).then(res => (res)).catch(err => {
+        console.error(err)
+        return err
+    })
+}
