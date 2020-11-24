18b9ea71ec0fdc7b0f1b4f961f3cba8ac70262a2
babaroga <mama@mia.com>
2020-11-24 03:13:34 -0500

    Updates

---
+++ precache-manifest.58294084959e019e84442a42e278a78d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b9475479243c23e428c0e5d8d4619d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c615c6da00246acf84b",
+    "url": "/static/css/2.20222f4f.chunk.css"
+  },
+  {
+    "revision": "e09bbeaa494d69dc951f",
+    "url": "/static/css/main.1300c07b.chunk.css"
+  },
+  {
+    "revision": "0c615c6da00246acf84b",
+    "url": "/static/js/2.f5cfdd84.chunk.js"
+  },
+  {
+    "revision": "f0b1a4eca1212b8883f983384622f5a3",
+    "url": "/static/js/2.f5cfdd84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09bbeaa494d69dc951f",
+    "url": "/static/js/main.67bac7d0.chunk.js"
+  },
+  {
+    "revision": "6d9d7c06d5fd2d1e3033",
+    "url": "/static/js/runtime-main.836d3ca0.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/static/media/primeicons.79432094.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vdora-client/precache-manifest.7301d21b51ecab8b1cd731748f58b658.js"
+  "/precache-manifest.58294084959e019e84442a42e278a78d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vdora-client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
