242a5cdb14a74c59f09084918f40130f4272d4a3
babaroga <mama@mia.com>
2020-12-09 18:39:53 -0500

    sorting

--- src/components/InventoryStockTable.tsx
+++ src/components/InventoryStockTable.tsx
@@ -16,7 +16,12 @@ export default function InventoryStockTable() {
   return (
     <>
       <Card title="Inventory Stock" className="m-4">
-        <DataTable value={stockData} className="">
+        <DataTable
+          value={stockData}
+          className=""
+          sortField="product__name"
+          sortOrder={-1}
+        >
           <Column field="product__name" header="Product" sortable />
           <Column field="In-house" header="In-house" />
           <Column field="Port Hope" header="Port Hope" />
